7fda1a27f0b631366cb592a260c1fda83322c415
Raghav Jhavar <raghav@jhavar.net>
2020-09-14 20:18:56 -0700

    Added argument get functionality

--- command.go
+++ command.go
@@ -1,6 +1,13 @@
 package commandparser
 
-import "strings"
+import (
+	"errors"
+	"strings"
+)
+
+var (
+	ErrArgOutOfBounds = errors.New("Argument index out of bounds")
+)
 
 type Command struct {
 	cmdStr    string
@@ -13,6 +20,37 @@ func NewCommand(cmdStr string) Command {
 		cmdStr:    cmdStr,
 		spaceIdxs: getSpaceIdxs(cmdStr),
 	}
+}
+
+//Returns the name of the command e.g.
+//command is "levelup 1", it returns "levelup"
+func (c *Command) Name() string {
+	//If there is no space, the command is
+	//argless i.e. just the name on its own
+	if len(c.spaceIdxs) == 0 {
+		return c.cmdStr
+	}
+
+	return c.cmdStr[:c.spaceIdxs[0]]
+}
+
+//Returns the arg at index i (starting at 0)
+//Error if out of bounds
+func (c *Command) Arg(i int) (string, error) {
+	if c.Size() == 0 || i < 0 || i >= c.Size() {
+		return "", ErrArgOutOfBounds
+	}
+
+	//Edge case for last argument
+	if i == c.Size()-1 {
+		return c.cmdStr[c.spaceIdxs[i]+1:], nil
+	}
+	return c.cmdStr[c.spaceIdxs[i]+1 : c.spaceIdxs[i+1]], nil
+}
+
+//Returns the number of args (not including name)
+func (c *Command) Size() int {
+	return len(c.spaceIdxs)
 }
 
 //Returns the indexes of each space in
